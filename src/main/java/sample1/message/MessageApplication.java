3f21673db147255992c13c95621d471cfb85d6a8
praveen polnati <praveenpolnati@gmail.com>
2025-01-06 12:18:13 +0530

    created war file

--- src/main/java/sample1/message/MessageApplication.java
+++ src/main/java/sample1/message/MessageApplication.java
@@ -2,12 +2,18 @@ package sample1.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MessageApplication {
+public class MessageApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MessageApplication.class);
+    }
 }
